124aaeb8be196bb08da0c2ed95960bac52eefb2a
AnatolyNikol <fanka4ectba@mail.ru>
2022-02-09 02:16:59 +0500

    Add lesson 2

---
+++ Lesson 2/js/script.js
@@ -0,0 +1,17 @@
+let a = 6;
+let b = 'Hello';
+
+console.log(a);
+
+let inputIn = document.querySelector('.input-in');
+let button = document.querySelector('button');
+let div = document.querySelector('.out');
+
+button.onclick = function () {
+    console.log('Работает!');
+    // console.log(inputIn.value);
+    let b = +inputIn.value; // + перевести в число!
+    console.log(b + 10);
+    div.innerHTML = b;
+    inputIn.value = '';
+}
